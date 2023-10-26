d74779770268555e740b2fe801b795df58b7e82c
Emanuel Leon-Vargas <146137985+EmanuelLeon2022@users.noreply.github.com>
2023-10-26 10:16:57 -0400

    Update homework.js
    "I among us, and you are among us, there are many people that live among us, everyone is among us"

--- homework.js
+++ homework.js
@@ -1,23 +1,24 @@
-class hamster{
-    constructor(owner, name, price){
-        this.owner = owner;
-        this.name = name;
-        this.price = price;
-    }
-    get wheelRun(){
-        console.log("squeak squeak");
-    }
-    get eatFood(){
-        console.log("nibble nibble");
-    }
-    get getPrice(){
-        return this.value();
-    }
-   value(){
-        return this.price;
-    }
-}
+// class hamster{
+//     constructor(owner, name, price){
+//         this.owner = owner;
+//         this.name = name;
+//         this.price = price;
+//     }
+//     get wheelRun(){
+//         console.log("squeak squeak");
+//     }
+//     get eatFood(){
+//         console.log("nibble nibble");
+//     }
+//     get getPrice(){
+//         return this.value();
+//     }
+//    value(){
+//         return this.price;
+//     }
+// }
 
 
-const Hampter = new hamster("Michelle", "Hampter", 15);
-console.log(Hampter.wheelRun);
+// const Hampter = new hamster("Michelle", "Hampter", 15);
+// console.log(Hampter.wheelRun);
+
